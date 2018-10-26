ee28914be1532319b2344db9285d5eba69d940cb
Sander Elias <sanderelias+git@gmail.com>
2018-10-26 18:02:49 +0200

    defunct app, but still shows issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testIt';
+
+  async swalShow() {
+    const result = await swal({
+      type: 'info',
+      title: 'test swal',
+      text: 'here comes some text',
+      showCancelButton: true
+    });
+    console.log(result);
+  }
 }
